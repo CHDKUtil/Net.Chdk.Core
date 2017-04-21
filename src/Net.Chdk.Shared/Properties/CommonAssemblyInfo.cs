0fa2a71e9c1df56a2ac8a6ba2138b17ab02e2d13
Dmitry Shechtman <damnian@gmail.com>
2017-04-21 17:36:18 +0300

    Add ComVisibleAttribute

--- src/Net.Chdk.Shared/Properties/CommonAssemblyInfo.cs
+++ src/Net.Chdk.Shared/Properties/CommonAssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Resources;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Dmitry Shechtman")]
 [assembly: AssemblyProduct("Net.Chdk")]
@@ -7,3 +8,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
+
+[assembly: ComVisible(false)]
