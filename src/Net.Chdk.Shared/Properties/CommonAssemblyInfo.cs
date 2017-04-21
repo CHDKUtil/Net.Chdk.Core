0cc747cfc972b956df43f58352ecdaecbe89c79c
Dmitry Shechtman <damnian@gmail.com>
2017-04-21 17:06:31 +0300

    Add Net.Chdk.Shared

---
+++ src/Net.Chdk.Shared/Properties/CommonAssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Resources;
+using System.Reflection;
+
+[assembly: AssemblyCompany("Dmitry Shechtman")]
+[assembly: AssemblyProduct("Net.Chdk")]
+[assembly: AssemblyCopyright("Copyright © 2017 Dmitry Shechtman")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
