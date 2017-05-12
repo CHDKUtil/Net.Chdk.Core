2c7a6329162e19e14bbd533ff13508d669d9378a
Dmitry Shechtman <damnian@gmail.com>
2017-05-12 06:59:48 +0300

    Add Directories and Files

---
+++ src/Net.Chdk.Core/Directories.cs
@@ -0,0 +1,8 @@
+﻿namespace Net.Chdk
+{
+    public static class Directories
+    {
+        public const string Images = "DCIM";
+        public const string Metadata = "_HDKMETA";
+    }
+}
---
+++ src/Net.Chdk.Core/Files.cs
@@ -0,0 +1,12 @@
+﻿namespace Net.Chdk
+{
+    public static class Files
+    {
+        public static class Metadata
+        {
+            public const string Software = "SOFTWARE.JSN";
+            public const string Camera = "CAMERA.JSN";
+            public const string CameraModel = "MODEL.JSN";
+        }
+    }
+}
