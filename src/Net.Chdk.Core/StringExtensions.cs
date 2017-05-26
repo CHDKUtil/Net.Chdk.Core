ed55a0b4bbccfa6e616e86bb8df1bede33957fc3
Dmitry Shechtman <damnian@gmail.com>
2017-05-26 13:01:51 +0300

    Add StringExtensions

---
+++ src/Net.Chdk.Core/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Net.Chdk
+{
+    public static class StringExtensions
+    {
+        public static string TrimStart(this string str, string prefix)
+        {
+            if (!str.StartsWith(prefix))
+                return null;
+            return str.Substring(prefix.Length);
+        }
+    }
+}
