22cbc340271552d1386ff2f22d86ddd1cb0cab4d
Dmitry Shechtman <damnian@gmail.com>
2017-05-29 15:30:42 +0300

    Obsolete Camera and CameraModel

--- src/Net.Chdk.Core/Files.cs
+++ src/Net.Chdk.Core/Files.cs
@@ -1,11 +1,17 @@
-﻿namespace Net.Chdk
+﻿using System;
+
+namespace Net.Chdk
 {
     public static class Files
     {
         public static class Metadata
         {
             public const string Software = "SOFTWARE.JSN";
+
+            [Obsolete]
             public const string Camera = "CAMERA.JSN";
+
+            [Obsolete]
             public const string CameraModel = "MODEL.JSN";
         }
     }
