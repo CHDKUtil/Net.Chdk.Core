c2047aebec1886c9d8af43d71e20278bd56b330b
Dmitry Shechtman <damnian@gmail.com>
2017-05-30 22:34:15 +0300

    Add Data, Category and Product to Directories

--- src/Net.Chdk.Core/Directories.cs
+++ src/Net.Chdk.Core/Directories.cs
@@ -4,5 +4,9 @@
     {
         public const string Images = "DCIM";
         public const string Metadata = "_HDKMETA";
+
+        public const string Data = "Data";
+        public const string Category = "Category";
+        public const string Product = "Product";
     }
 }
